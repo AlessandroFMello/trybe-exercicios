f148d12256041cfc606668b5a0523c474c1d7ad7
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-10 18:46:56 -0300

    adiciona o desafio 4

--- modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
+++ modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
@@ -12,3 +12,8 @@ document.body.appendChild(tagMain);
 let tagSection = document.createElement('section');
 tagSection.className = 'center-content';
 tagMain.appendChild(tagSection);
+
+//DESAFIO 4:
+let tagP = document.createElement('p');
+tagP.innerText = 'Colocar algum texto relevante aqui'
+tagSection.appendChild(tagP);
