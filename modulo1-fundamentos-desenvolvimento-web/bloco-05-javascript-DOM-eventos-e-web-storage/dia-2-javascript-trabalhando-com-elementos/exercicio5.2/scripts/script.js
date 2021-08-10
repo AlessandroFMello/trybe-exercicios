2d733c57d8863a7b7161f55c265792d0632383a5
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-10 18:44:49 -0300

    Adiciona o desafio 3

--- modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
+++ modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
@@ -4,3 +4,11 @@ tagH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(tagH1);
 
 //DESAFIO 2:
+let tagMain = document.createElement('main');
+tagMain.className = 'main-content';
+document.body.appendChild(tagMain);
+
+//DESAFIO 3:
+let tagSection = document.createElement('section');
+tagSection.className = 'center-content';
+tagMain.appendChild(tagSection);
