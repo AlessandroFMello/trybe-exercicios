b2eaa223a442f466ac0b1976da427a4fd8fd10de
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-10 19:09:17 -0300

    adiciona o desafio 9

--- modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
+++ modulo1-fundamentos-desenvolvimento-web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio5.2/scripts/script.js
@@ -47,3 +47,9 @@ for (let number in numExtenso) {
   listaUl.appendChild(li);
 }
 rightSection.appendChild(listaUl);
+
+//DESAFIO 9:
+for (let index = 0; index < 3; index += 1) {
+  let h3 = document.createElement('h3');
+  tagMain.appendChild(h3);
+}
