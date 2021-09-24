e5e6e1dfc6a7a22045479142892befdce3aa2cfd
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-23 23:02:44 -0300

    Adiciona a resoluçao do exeercicio 6

--- modulo1-fundamentos-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/exercicio06/exercicio06.js
+++ modulo1-fundamentos-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/exercicio06/exercicio06.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch');
+const API_URL = 'https://dog.ceo/api/breeds/image/random';
+
 // Crie uma função que faça requisição para a api dog pictures.
 // Mocke a requisição e crie dois testes.
 // O primeiro deve interpretar que a requisição se resolveu e
@@ -6,3 +9,14 @@
 // como valor "request failed".
 // Crie todos os testes que achar necessário.
 
+const fetchedUrl = async (url) => {
+  try {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((json) => json);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = { fetchedUrl }
